3d77c2c54eabcb4d5e9d55a38184594e21036026
Alex Holovatsky <82158670+AlexHolovatsky@users.noreply.github.com>
2021-12-09 18:47:56 +0200

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = var.aws_reg
+  # version = "2.12.0"
+  profile = "terraform"
+}
+
+terraform {
+  required_providers {
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.1.2"
+    }
+          ibm = {
+      source = "ibm-cloud/ibm"
+    }
+  }
+}
