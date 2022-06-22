f7c349fd0125cda25190243e39b37f4fb05a48a4
Alex Holovatsky <82158670+AlexHolovatsky@users.noreply.github.com>
2022-06-22 23:57:46 +0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,17 +1,3 @@
 provider "aws" {
   region = var.aws_reg
-  # version = "2.12.0"
-  profile = "terraform"
-}
-
-terraform {
-  required_providers {
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.1.2"
-    }
-          ibm = {
-      source = "ibm-cloud/ibm"
-    }
-  }
 }
